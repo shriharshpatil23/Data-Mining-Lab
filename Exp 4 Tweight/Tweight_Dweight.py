1d91c7d285fd6b130c6da671f24969252b861f05
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-09-30 08:38:53 +0530

    formatting

--- Exp 4 Tweight/Tweight_Dweight.py
+++ Exp 4 Tweight/Tweight_Dweight.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import csv
 data = pd.read_csv('Tweight_Dweight_Input.csv')
@@ -7,7 +6,6 @@ Location = data['Location'].tolist()
 A = data['A'].tolist()
 B = data['B'].tolist()    
 Total = []
-
 for i in range(len(A)):
     tmp = A[i]+B[i]
     Total.append(tmp)
@@ -17,15 +15,21 @@ A_T_wt = []
 B_T_wt = []
 A_D_wt = []
 B_D_wt = []
+verify=[]
 sum_total = sum(Total)
+A_total=sum(A)
+B_total=sum(B)
+
 
 for i in range(len(A)):
     A_T_wt.append(A[i]/Total[i])
     B_T_wt.append(B[i]/Total[i])
-    A_D_wt.append(A[i]/sum_total)
-    B_D_wt.append(B[i]/sum_total)
-
-header = ['Location', 'A', 'B', 'Total' , 'A_T_wt','B_T_wt','A_D_wt','B_D_wt']
+    A_D_wt.append(A[i]/A_total)
+    B_D_wt.append(B[i]/B_total)
+    verify.append(A_T_wt[i]+B_T_wt[i])
+print("A D_weight total -> {}".format(sum(A_D_wt)))
+print("B D_weight total -> {}".format(sum(B_D_wt)))
+header = ['Location', 'A', 'B', 'Total' , 'A_T_wt','B_T_wt','verify t weight','A_D_wt','B_D_wt']
 
 
 
@@ -38,5 +42,5 @@ with open('Tweight_Dweight_Output.csv', 'w', encoding='UTF8') as f:
     # write the data
 
     for i in range(len(A)):
-        data = [Location[i],A[i],B[i],Total[i],A_T_wt[i],B_T_wt[i],A_D_wt[i],B_D_wt[i]]
+        data = [Location[i],A[i],B[i],Total[i],A_T_wt[i],B_T_wt[i],verify[i],A_D_wt[i],B_D_wt[i]]
         writer.writerow(data)
